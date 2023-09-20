ef40916c21e96fe39d6f974e7606f5448287ee5a
Ewerton Eiti Yamada <ewerton.yamada@sgsistemas.com.br>
2023-09-20 14:45:24 -0300

    codereview

--- src/main/java/com/cbf/championship/classifications_table/repository/ClassificationTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cbf.championship.classifications_table.repository;
-
-import com.cbf.championship.classifications_table.ClassificationsTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClassificationTableRepository extends JpaRepository<ClassificationsTable, Integer> {
-}
